c4d3e9f5208785bc03145c35e68484e415e7dc1d
Pablo Sierra <pavels.jo@gmail.com>
2021-03-26 11:56:24 -0300

    Button removed and ctrl + enter feature added

--- main/notebook.js
+++ main/notebook.js
@@ -3,18 +3,22 @@ $(document).ready( () => {
     // DOM elements
     var myTextArea = $(".notebook textarea");
     var editor = CodeMirror.fromTextArea(myTextArea[0], 
-        {mode: 'text/x-mysql',
-        lineNumbers: true});
+        {
+            mode: 'text/x-mysql',
+            lineNumbers: true
+        }
+    );
 
     // DOM interaction
-    $(".notebook-cell-button").on('click', () => {
-
-        db.transaction((tran) => {
-            tran.executeSql(editor.getValue(), [], (tran, data) => {
-                console.log(data);
-            }, (err)=>{
-                console.log(err);
+    $(".notebook-cell").on('keydown', (e) => {
+        
+        if (e.ctrlKey && e.keyCode === 13) {
+            db.transaction((tran) => {
+                tran.executeSql(editor.getValue(), [], (tran, data) => {
+                    console.log(data.rows);
+                });
             });
-        });
+        }
+        
     });
 });
